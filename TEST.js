11854cc2aca413850e62cabd20bbf2bfc5996eeb
KestrelP <ra7777@gmail.com>
2023-09-27 21:21:19 +0300

    updoot

---
+++ TEST.js
@@ -0,0 +1,3 @@
+let test = "https://www.youtube.com/watch?v=0n809nd4Zu4&t=1541s&ab_channel=freeCodeCamp.org"
+
+console.log(test.split("?")[1]);
--- background.js
+++ background.js
@@ -1,3 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if(tab.url && tab.url.includes(""))
+    if(tab.url && tab.url.includes("youtube.com/watch")){
+        const queryParameters = tab.url.split("?")[1];
+        const ulrParameters = new URLSearchParams(queryParameters);
+
+        chrome.tabs.sendMessage(tabId)
+    }
 })
